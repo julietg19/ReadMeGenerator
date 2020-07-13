ee50f580074811a5e3da5ad723d8c94a677d7b98
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-07-13 11:23:33 -0500

    questions

--- index.js
+++ index.js
@@ -9,9 +9,60 @@ inquirer
       message: "What is the title of your project?",
       name: "title",
     },
+    {
+      type: "input",
+      message: "What is the description of your project?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What is the Table of Contents of your project?",
+      name: "content",
+    },
+    {
+      type: "input",
+      message: "What packages are you using for your project?",
+      name: "packages",
+    },
+    {
+      type: "input",
+      message: "what are the licensing requirements for this project?",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Who is contributing to this project?",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "What tests are you using for this project?",
+      name: "tests",
+    },
+
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "question",
+    },
+    {
+      type: "input",
+      message: "What is the title of your badge?",
+      name: "badge",
+    },
+    {
+      type: "input",
+      message: "What is the message of your badge?",
+      name: "message",
+    },
+    {
+      type: "input",
+      message: "What is the color of your badge?",
+      name: "color",
+    },
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    console.log(answers);
   })
   .catch((error) => {
     if (error.isTtyError) {
