951949adb6abe061db5e20729e61ccf19615157d
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-07-13 11:09:27 -0500

    npms

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
